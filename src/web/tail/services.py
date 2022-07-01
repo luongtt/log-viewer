22da2a04189fccdc663a5e9f61d0a4ed56f71936
luongtt <trantienluong97@gmail.com>
2022-07-01 19:36:22 +0700

    don't use  in loop

--- src/web/tail/services.py
+++ src/web/tail/services.py
@@ -4,7 +4,7 @@ from src.web.config.app import AppConfig
 
 def tail(app, socket_io, namespace):
     f = subprocess.Popen(['tail', '-F', AppConfig.FILE_NAME], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    while True:
-        line = f.stdout.readline()
-        with app.app_context():
+    with app.app_context():
+        while True:
+            line = f.stdout.readline()
             socket_io.emit("line", line.decode("utf-8"), namespace=namespace)
